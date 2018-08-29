99f92f2292e58047b4dfa1a87fdf3ec958da5bcd
Marian-Amoah <42767628+Marian-Amoah@users.noreply.github.com>
2018-08-29 21:47:53 +0000

    Update assignment2.cc

--- assignment2.cc
+++ assignment2.cc
@@ -1,3 +1,32 @@
 #include<iostream>
 using namespace std;
-int
+int main(){
+  int input;
+  bool isPrime = true;
+  cout << " input a positive number " << endl;
+  cin >> input;
+  if(input == 0)
+  {
+    cout << " This is not a prime Number " << endl;
+  }
+  else
+  {
+    for(int i = 2; i <= input /2; i++)
+    {
+      if(input % i ==0)
+      {
+        isPrime = false;
+        break;
+      }
+    }
+    if(isPrime)
+    {
+      cout << " This is a prime Number " << endl;
+    }
+    else
+    {
+      cout << " This is not a prime Number " << endl;
+    }
+  }
+  return 0;
+}
